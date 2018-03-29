c2a4683d0770d194775d3aad3a1b4d9ef81fa3be
JChandler81 <jared.chandler1@gmail.com>
2018-03-29 18:35:32 -0500

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] passcodeList = new string[2];
+            passcodeList[0] = "mywsuId123";
+            passcodeList[1] = "mywsuId890";
+            passcodeList[2] = "mywsuId321";
+            passcodeList[3] = "mywsuId192";
+            passcodeList[4] = "mywsuId430";
+
+            Write("Please enter passcode to access the system: ");
+
+            string userPasscode = ReadLine();
+            bool authenticated = false; //To check if authenticated
+
+            foreach (string passcode in passcodeList)
+            {
+                if (userPasscode == passcode)
+                {
+                    //Set authentication to true
+                    authenticated = true;
+                }
+            }
+
+
+            if (authenticated) //if authenticated is true
+            {
+                string userName = string.Empty;
+                Write("Enter user name:");
+                userName = ReadLine();
+
+                WriteLine($"UserName: {userName}. You are successfully authenticated by the system.");
+
+            }
+            else //if authenticated is false
+            {
+                WriteLine("You have entered incorrect passcode. Please try again.");
+                ReadLine();
+            }
+        }
+    }
+
+}
