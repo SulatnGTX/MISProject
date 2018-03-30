59792b8a438f6bcad85a58327259f0dbdd711419
SulatnGTX <37317446+SulatnGTX@users.noreply.github.com>
2018-03-30 16:03:13 -0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,48 +1,68 @@
-﻿using static System.Console;
+using System;
+using static System.Console;
+using static System.Convert;
 
-namespace Assignment3
+namespace Project1_P1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] passcodeList = new string[2];
-            passcodeList[0] = "mywsuId123";
-            passcodeList[1] = "mywsuId890";
-            passcodeList[2] = "mywsuId321";
-            passcodeList[3] = "mywsuId192";
-            passcodeList[4] = "mywsuId430";
+            WriteLine("Chose from the options:");
+            WriteLine();
+            WriteLine("New Cosumter = 1, New Product = 2, Order/Sales = 3, Search Csutomer ID = 4, Search Csutomer Name =5, Search Product ID = 5, Search Product Name = 6, Calculate total sales by product = 7, Calculate total sales for customer by entering name =8");
+            WriteLine();
+            WriteLine("Enter your option:");
+            int Info = ToInt32(ReadLine());
 
-            Write("Please enter passcode to access the system: ");
-
-            string userPasscode = ReadLine();
-            bool authenticated = false; //To check if authenticated
-
-            foreach (string passcode in passcodeList)
+            if(Info == 1)
             {
-                if (userPasscode == passcode)
-                {
-                    //Set authentication to true
-                    authenticated = true;
-                }
+                WriteLine("The Customer information");
+                WriteLine();
+                WriteLine("The Customer's ID:");
+                int CID = ToInt32(ReadLine());
+                WriteLine();
+                WriteLine("The Customer's First and Last Name:");
+                String CName = ReadLine();
+                WriteLine();
+                WriteLine("The Customer's Email:");
+                String CEmail = ReadLine();
+                WriteLine();
+                WriteLine("The Customer's Address:");
+                String CAddress = ReadLine();
+                WriteLine();
+                WriteLine("The Customer's Phone Number in this format (###)###-### :");
+                String CPhoneNumber = ReadLine();
+            }
+            else if(Info == 2)
+            {
+                WriteLine("The Product information");
+                WriteLine();
+                WriteLine("The Product ID:");
+                int PID = ToInt32(ReadLine());
+                WriteLine();
+                WriteLine("The Product Name:");
+                String PName = ReadLine();
+                WriteLine();
+                WriteLine("The Product Category:");
+                String PCategory = ReadLine();
+                WriteLine();
+                WriteLine("The Product Description:");
+                String PDescription = ReadLine();
+                WriteLine();
+                WriteLine("The Price(don't write the $ sgin):");
+                int PPrice = ToInt32(ReadLine());
+            }
+            else if(Info == 3)
+            {
+                WriteLine("Enter the Customer ID:");
+                int CustomerID = ToInt32(ReadLine());
+                WriteLine();
+                WriteLine("Enter the Product ID:");
+                int ProductID = ToInt32(ReadLine());
+                WriteLine();
             }
 
-
-            if (authenticated) //if authenticated is true
-            {
-                string userName = string.Empty;
-                Write("Enter user name:");
-                userName = ReadLine();
-
-                WriteLine($"UserName: {userName}. You are successfully authenticated by the system.");
-
-            }
-            else //if authenticated is false
-            {
-                WriteLine("You have entered incorrect passcode. Please try again.");
-                ReadLine();
-            }
         }
     }
-
 }
