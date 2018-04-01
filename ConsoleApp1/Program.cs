3718fdea91f9e47200c9d93c90cfd72182fc436b
SulatnGTX <37317446+SulatnGTX@users.noreply.github.com>
2018-03-31 19:24:10 -0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using static System.Console;
 using static System.Convert;
-using ProjectMIS310;
+using System.Collections.Generic;
 
 namespace Project_1
 {
@@ -17,6 +17,28 @@ namespace Project_1
             CustomerID[3] = "0004";
             CustomerID[4] = "0005";
 
+            List<Customer> customers = new List<Customer>();
+
+            Customer cust1 = new Customer();
+            cust1.ID = "0001";
+            cust1.Name = "Foo";
+            cust1.Phone = "316 555 5050";
+            cust1.Email = "foo@gmail.com";
+            cust1.Address = "555 Mulberry Ln, Wichita, KS 67212";
+
+            customers.Add(cust1);
+
+            Customer cust2 = new Customer();
+            cust2.ID = "0002";
+            cust2.Name = "Pho";
+            cust2.Phone = "316 555 1010";
+            cust2.Email = "pho@gmail.com";
+            cust2.Address = "532 Mulberry Ln, Wichita, KS 67212";
+
+            customers.Add(cust2);
+
+
+
             string[] ProductID = new string[5];
             ProductID[0] = "0001";
             ProductID[1] = "0002";
@@ -24,10 +46,21 @@ namespace Project_1
             ProductID[3] = "0004";
             ProductID[4] = "0005";
 
+            List<Product> Products = new List<Product>();
+
+            Product prod1 = new Product();
+            prod1.ID = "0001";
+            prod1.Name = "iPhone";
+            prod1.Category = "Phone";
+            prod1.Description = "smartphone";
+            prod1.Price = "1099";
+
+            Products.Add(prod1);
+
 
             WriteLine("Chose from the options:");
             WriteLine();
-            WriteLine("New Cosumter = 1, New Product = 2, Order/Sales = 3, Search Csutomer ID = 4, Search Csutomer Name =5, Search Product ID = 5, Search Product Name = 6, Calculate total sales by product = 7, Calculate total sales for customer by entering name =8");
+            WriteLine("New Cosumter = 1, New Product = 2, Order/Sales = 3, Search Csutomer ID = 4, Search Csutomer Name =5, Search Product ID = 6, Search Product Name = 7, Calculate total sales by product = 8, Calculate total sales for customer by entering name =9");
             WriteLine();
             WriteLine("Enter your option:");
             int Info = ToInt32(ReadLine());
@@ -37,7 +70,7 @@ namespace Project_1
                 WriteLine("The Customer information");
                 WriteLine();
                 WriteLine("The Customer's ID:");
-                int CID = ToInt32(ReadLine());
+                String CID = ReadLine();
                 WriteLine();
                 WriteLine("The Customer's First and Last Name:");
                 String CName = ReadLine();
@@ -58,7 +91,7 @@ namespace Project_1
                 WriteLine("The Product information");
                 WriteLine();
                 WriteLine("The Product ID:");
-                int PID = ToInt32(ReadLine());
+                String PID = ReadLine();
                 WriteLine();
                 WriteLine("The Product Name:");
                 String PName = ReadLine();
@@ -70,7 +103,7 @@ namespace Project_1
                 String PDescription = ReadLine();
                 WriteLine();
                 WriteLine("The Price(don't write the $ sgin):");
-                int PPrice = ToInt32(ReadLine());
+                String PPrice = ReadLine();
             }
 
 
@@ -101,32 +134,77 @@ namespace Project_1
                 string Customer = ReadLine();
                 bool authenticated = false;
 
-                foreach (string CI in CustomerID)
+                foreach (var customer in customers)
                 {
-                    if (Customer == CI)
+                    if (customer.ID == Customer)
                     {
-
+                        WriteLine($"{customer.ID}");
+                        WriteLine($"{customer.Name}");
+                        WriteLine($"{customer.Phone}");
+                        WriteLine($"{customer.Email}");
+                        WriteLine($"{customer.Address}");
                         authenticated = true;
                     }
                 }
 
-                if (authenticated)
-                {
-                    string Name = string.Empty;
-                    WriteLine("Enter Customer Name:");
-                    Name = ReadLine();
-
-                    if (Name != string.Empty)
-                    {
-                        WriteLine($"CustomerID: {Name}. You are successfully authenticated by the system.");
-                    }
-
-                }
-
-                else
+                if (!authenticated)
                 {
                     WriteLine("You have entered incorrect CustomerID. Please try again.");
+
                 }
+            }
+            else if(Info == 5)
+            {
+                WriteLine("Enter CustomerName:");
+                string Customer = ReadLine();
+                bool authenticated = false;
+
+                foreach (var customer in customers)
+                {
+                    if (customer.Name == Customer)
+                    {
+                        WriteLine($"{customer.ID}");
+                        WriteLine($"{customer.Name}");
+                        WriteLine($"{customer.Phone}");
+                        WriteLine($"{customer.Email}");
+                        WriteLine($"{customer.Address}");
+                        authenticated = true;
+                    }
+                }
+
+                if (!authenticated)
+                {
+                    WriteLine("You have entered incorrect CustomerID. Please try again.");
+
+                }
+            }
+
+            else if(Info == 6)
+            {
+
+                    WriteLine("Enter ProductID:");
+                    string Product = ReadLine();
+                    bool authenticated = false;
+
+                    foreach (var product in Products)
+                    {
+                        if (product.ID == Product)
+                        {
+                        WriteLine($"{product.ID}");
+                        WriteLine($"{product.Name}");
+                        WriteLine($"{product.Category}");
+                        WriteLine($"{product.Description}");
+                        WriteLine($"{product.Price}");
+                            authenticated = true;
+                        }
+                    }
+
+                    if (!authenticated)
+                    {
+                        WriteLine("You have entered incorrect ProductID. Please try again.");
+
+                    }
+
             }
 
         }
