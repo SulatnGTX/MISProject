9bf1b0c529a06e8014010114af971b334a1c8ca3
SulatnGTX <37317446+SulatnGTX@users.noreply.github.com>
2018-03-31 19:26:11 -0500

    Add files via upload

---
+++ ConsoleApp1/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Project_1
+{
+    public class Customer
+    {
+        public string ID;
+        public string Name;
+        public string Phone;
+        public string Email;
+        public string Address;
+    }
+}
---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Project_1
+{
+    public class Product
+    {
+        public string ID;
+        public string Name;
+        public string Category;
+        public string Description;
+        public string Price;
+    }
+}
